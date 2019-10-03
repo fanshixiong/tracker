e384ee45ffa30221546bb2f26a541395be08cc1c
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2019-10-03 10:52:22 +0800

    This is a tracker

---
+++ tracker.m
@@ -0,0 +1,162 @@
+function [positions, time] = tracker(video_path, img_files, pos, target_sz, ...
+	padding, kernel, lambda, output_sigma_factor, interp_factor, cell_size, ...
+	features, show_visualization)
+%TRACKER Kernelized/Dual Correlation Filter (KCF/DCF) tracking.
+%   This function implements the pipeline for tracking with the KCF (by
+%   choosing a non-linear kernel) and DCF (by choosing a linear kernel).
+%
+%   It is meant to be called by the interface function RUN_TRACKER, which
+%   sets up the parameters and loads the video information.
+%
+%   Parameters:
+%     VIDEO_PATH is the location of the image files (must end with a slash
+%      '/' or '\').
+%     IMG_FILES is a cell array of image file names.
+%     POS and TARGET_SZ are the initial position and size of the target
+%      (both in format [rows, columns]).
+%     PADDING is the additional tracked region, for context, relative to 
+%      the target size.
+%     KERNEL is a struct describing the kernel. The field TYPE must be one
+%      of 'gaussian', 'polynomial' or 'linear'. The optional fields SIGMA,
+%      POLY_A and POLY_B are the parameters for the Gaussian and Polynomial
+%      kernels.
+%     OUTPUT_SIGMA_FACTOR is the spatial bandwidth of the regression
+%      target, relative to the target size.
+%     INTERP_FACTOR is the adaptation rate of the tracker.
+%     CELL_SIZE is the number of pixels per cell (must be 1 if using raw
+%      pixels).
+%     FEATURES is a struct describing the used features (see GET_FEATURES).
+%     SHOW_VISUALIZATION will show an interactive video if set to true.
+%
+%   Outputs:
+%    POSITIONS is an Nx2 matrix of target positions over time (in the
+%     format [rows, columns]).
+%    TIME is the tracker execution time, without video loading/rendering.
+%
+%   Joao F. Henriques, 2014
+
+
+	%if the target is large, lower the resolution, we don't need that much
+	%detail  确定尺寸
+	resize_image = (sqrt(prod(target_sz)) >= 100);  %diagonal size >= threshold
+	if resize_image,
+		pos = floor(pos / 2);
+		target_sz = floor(target_sz / 2);
+	end
+
+
+	%window size, taking padding into account 确定检测范围
+	window_sz = floor(target_sz * (1 + padding));
+	
+% 	%we could choose a size that is a power of two, for better FFT
+% 	%performance. in practice it is slower, due to the larger window size.
+% 	window_sz = 2 .^ nextpow2(window_sz);
+
+	
+	%create regression labels, gaussian shaped, with a bandwidth
+	%proportional to target size % output_sigma 高斯标签的带宽
+	output_sigma = sqrt(prod(target_sz)) * output_sigma_factor / cell_size;
+	yf = fft2(gaussian_shaped_labels(output_sigma, floor(window_sz / cell_size)));
+    % gaussian_shaped_labels 生成回归标签 在进行傅里叶变换转换到频域上的y （f）
+	%store pre-computed cosine window 汉宁窗 尺寸与y（f）同，floor(indow_sz/cell_sz)
+	cos_window = hann(size(yf,1)) * hann(size(yf,2))';	
+	%检测范围window_sz中cell的个数
+	%利用show_window将跟踪结果可视化
+	if show_visualization,  %create video interface
+		update_visualization = show_video(img_files, video_path, resize_image);
+	end
+	
+	
+	%note: variables ending with 'f' are in the Fourier domain.
+
+	time = 0;  %to calculate FPS
+	positions = zeros(numel(img_files), 2);  %to calculate precision每帧目标的中心位置
+
+	for frame = 1:numel(img_files), %逐帧进行跟踪
+		%load image
+		im = imread([video_path img_files{frame}]);
+		if size(im,3) > 1,
+			im = rgb2gray(im);
+		end
+		if resize_image,
+			im = imresize(im, 0.5);
+		end
+
+		tic()
+
+		if frame > 1,
+			%obtain a subwindow for detection at the position from last
+			%frame, and convert to Fourier domain (its size is unchanged)
+			patch = get_subwindow(im, pos, window_sz); % 在上一帧跟踪结果pos基础上，根据window_sz在这一帧上提取检测区域patch这一帧的训练，与下一帧的检测，使用的是用一块patch
+			zf = fft2(get_features(patch, features, cell_size, cos_window));
+			% 利用get_features获得检测区域patch的特征zf
+			%calculate response of the classifier at all shifts
+			switch kernel.type
+			case 'gaussian',%利用zf和model_xf计算高斯响应
+				kzf = gaussian_correlation(zf, model_xf, kernel.sigma);
+			case 'polynomial',
+				kzf = polynomial_correlation(zf, model_xf, kernel.poly_a, kernel.poly_b);
+			case 'linear',
+				kzf = linear_correlation(zf, model_xf);
+			end
+			response = real(ifft2(model_alphaf .* kzf));  %equation for fast detection
+            % 点乘后进行傅里叶反变换，回到时域。保留实部得到实数响应图，并且响应均为归一化值。
+			%target location is at the maximum response. we must take into
+			%account the fact that, if the target doesn't move, the peak
+			%will appear at the top-left corner, not at the center (this is
+			%discussed in the paper). the responses wrap around cyclically.
+			[vert_delta, horiz_delta] = find(response == max(response(:)), 1);
+			if vert_delta > size(zf,1) / 2,  %wrap around to negative half-space of vertical axis
+				vert_delta = vert_delta - size(zf,1);
+			end
+			if horiz_delta > size(zf,2) / 2,  %same for horizontal axis
+				horiz_delta = horiz_delta - size(zf,2);
+			end
+			pos = pos + cell_size * [vert_delta - 1, horiz_delta - 1];
+		end
+
+		%obtain a subwindow for training at newly estimated target position
+		patch = get_subwindow(im, pos, window_sz);
+		xf = fft2(get_features(patch, features, cell_size, cos_window));
+        %get_features 获得第一帧patch的特征矩阵，再经过傅里叶变换到频域得到xf
+		%Kernel Ridge Regression, calculate alphas (in Fourier domain)
+		switch kernel.type
+		case 'gaussian',
+			kf = gaussian_correlation(xf, xf, kernel.sigma); %得到频域上的高斯响应kf
+		case 'polynomial',
+			kf = polynomial_correlation(xf, xf, kernel.poly_a, kernel.poly_b);
+		case 'linear',
+			kf = linear_correlation(xf, xf);
+		end
+		alphaf = yf ./ (kf + lambda);   %equation for fast training 岭回归计算，得到分类器参数α
+
+		if frame == 1,  %first frame, train with a single image
+			model_alphaf = alphaf;
+			model_xf = xf;    %将得到的α和xf作为第一帧时分类器训练的结果，初始化结束
+		else
+			%subsequent frames, interpolate model 更新两个变量
+			model_alphaf = (1 - interp_factor) * model_alphaf + interp_factor * alphaf;
+			model_xf = (1 - interp_factor) * model_xf + interp_factor * xf;
+		end
+
+		%save position and timing
+		positions(frame,:) = pos;
+		time = time + toc();
+
+		%visualization可视化
+		if show_visualization,
+			box = [pos([2,1]) - target_sz([2,1])/2, target_sz([2,1])];
+			stop = update_visualization(frame, box);
+			if stop, break, end  %user pressed Esc, stop early
+			
+			drawnow
+% 			pause(0.05)  %uncomment to run slower
+		end
+		
+	end
+
+	if resize_image,
+		positions = positions * 2;
+	end
+end
+
